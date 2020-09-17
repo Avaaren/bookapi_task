587b06048cd33f67e6b55d4e53281abad0950892
Avaaren <avaaren@mail.ru>
2020-09-17 21:10:30 +0300

    Added book list view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView
+)
+from rest_framework.response import Response
+
+from .models import Book
+from .serializers import (
+    BookListSerializer,
+)
+
+
+class BookListView(ListAPIView):
+    '''Отображение списка книг'''
+    queryset = Book.objects.all()
+    serializer_class = BookListSerializer
+
+    def get(self, request):
+        '''Отображение списка всех книг'''
+        books = Book.objects.all()
+        serializer = BookListSerializer(books, many=True)
+        return Response(serializer.data)
