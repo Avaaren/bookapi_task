7c3f2d57994610e1bd1c63ea5d9451e28dc710fc
Avaaren <avaaren@mail.ru>
2020-09-17 20:34:16 +0300

    Created books model

--- books/admin.py
+++ books/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        list_display = '__all__'
--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField("Название книги", max_length=35)
+    author_name = models.CharField("Имя автора", max_length=50)
+
+    def __str__(self):
+        return f"{self.title} by {self.author_name}"
+
+    class Meta:
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
